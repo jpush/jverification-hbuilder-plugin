03e1ebf3f82fa8e15d0b1f73d377a27b51410358
刘沛荣 <liupr@jiguang.cn>
2023-02-13 16:40:28 +0800

    xcode14兼容xcode13

---
+++ UniPlugin-JVerification-iOS/HBuilder-uniPluginDemo/UniPluginJVerification/buildFramework.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ 
+BIN_DIR="bin"
+if [ ! -d "$BIN_DIR" ]; then
+mkdir -p "$BIN_DIR"
+fi
+ 
+BIN_DIR_TMP="bin_tmp"
+if [ ! -d "$BIN_DIR_TMP" ]; then
+mkdir -p "$BIN_DIR_TMP"
+fi
+ 
+ 
+ 
+cp -af ${BUILT_PRODUCTS_DIR}/${TARGET_NAME}.framework/ ${BIN_DIR_TMP}/${PLATFORM_NAME}-${TARGET_NAME}.framework
+cp -af ${BUILT_PRODUCTS_DIR}/${TARGET_NAME}.framework/ ${BIN_DIR}/${TARGET_NAME}.framework
+lipo -create $BIN_DIR_TMP/*-${TARGET_NAME}.framework/${TARGET_NAME} -output ${BIN_DIR}/${TARGET_NAME}.framework/${TARGET_NAME}
